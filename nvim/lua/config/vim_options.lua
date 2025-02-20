6e43c4db2c538a8dab3c62580022a6180fb9e9fb
Shekhar S <shekharshashank97@gmail.com>
2025-02-20 21:07:39 +0530

    nvim config
    Added vim opions

--- nvim/lua/config/vim_options.lua
+++ nvim/lua/config/vim_options.lua
@@ -19,3 +19,31 @@ vim.cmd("set clipboard=unnamedplus")
 
 -- views can only be fully collapsed with the global statusline
 vim.opt.laststatus = 3
+
+
+
+-- You will likely want to reduce updatetime which affects CursorHold
+-- note: this setting is global and should be set only once
+vim.o.updatetime = 250
+vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+  group = vim.api.nvim_create_augroup("float_diagnostic", { clear = true }),
+  callback = function ()
+    vim.diagnostic.open_float(nil, {focus=false})
+  end
+})
+
+vim.api.nvim_create_autocmd("CursorHold", {
+  buffer = buffr,
+  callback = function()
+    local opts = {
+      focusable = false,
+      close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+      border = 'rounded',
+      source = 'always',
+      prefix = ' ',
+      scope = 'cursor',
+    }
+    vim.diagnostic.open_float(nil, opts)
+  end
+})
+
