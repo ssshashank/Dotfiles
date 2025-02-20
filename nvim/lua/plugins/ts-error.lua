3ab4f8b3b90f95796fdd88e91a76c31d91df8052
Shekhar S <shekharshashank97@gmail.com>
2025-02-20 21:08:05 +0530

    Plugins
    Added plugins
    Added LSP configs

---
+++ nvim/lua/plugins/ts-error.lua
@@ -0,0 +1,9 @@
+-- stable version
+return {
+  "dmmulroy/ts-error-translator.nvim",
+  config = function()
+    require("ts-error-translator").setup({
+      auto_override_publish_diagnostics = true
+    })
+  end
+}
